b673599f0737cf13a7b2d37ca567bc591735cf2c
snehapriya-bs <152855542+snehapriya-bs@users.noreply.github.com>
2024-07-07 08:08:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-import logging
-
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World'
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=5000)
